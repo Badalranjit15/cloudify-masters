2cc55e6716f76219bc5f143d7602bfcbf87ad251
Badalranjit15 <65303330+Badalranjit15@users.noreply.github.com>
2022-04-15 16:35:58 +0530

    Update main.tf

--- exl-poc-main/vpc/module/main.tf
+++ exl-poc-main/vpc/module/main.tf
@@ -6,7 +6,6 @@ module "vpc" {
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
 
   enable_nat_gateway = var.enable_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
